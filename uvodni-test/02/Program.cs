d4facf25e1d15725ce3ebb6900b2a53dd47e402c
theonlydejf <krcmar.dav@gmail.com>
2024-09-12 21:53:49 +0200

    add solution for question 2

---
+++ uvodni-test/02/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _02;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //// Nacitani vstupu ////
+        // Prvni cislo
+        Console.WriteLine("Zadej prvni cislo:"); // Napise uzivateli co ma udelat
+        string cislo1Text = Console.ReadLine(); // Nacte hodnotu od uzivatele
+        double cislo1 = Convert.ToDouble(cislo1Text); // Prevede textovou hodotu na cislo
+        // Druhe cislo
+        Console.WriteLine("Zadej druhe cislo:"); // Napise uzivateli co ma udelat
+        string cislo2Text = Console.ReadLine(); // Nacte hodnotu od uzivatele
+        double cislo2 = Convert.ToDouble(cislo2Text); // Prevede textovoud hodotu na cislo
+
+        //// Spocitani vysledku ////
+        // Spocitani vysledku
+        double vysledek = cislo1 / cislo2;
+        // Vypsani vysledku
+        Console.WriteLine("Podil:");
+        Console.WriteLine(vysledek);
+    }
+}
