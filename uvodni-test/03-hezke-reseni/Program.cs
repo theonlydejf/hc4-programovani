739b8c5a75fd3729ac8ccf5896d89f1a6eeb0287
theonlydejf <krcmar.dav@gmail.com>
2024-09-17 13:12:33 +0200

    add solution to question 3

---
+++ uvodni-test/03-hezke-reseni/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _03_hezke_reseni;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Zname hracky
+        string[] hracky = new string[] { "LEGO stavebnice", "Hot Wheels auticko", "Panenka" };
+        int[] ceny = new int[] { 200, 75, 120 };
+
+        Console.WriteLine("Kolik mas penez?");
+        double penize = Convert.ToDouble(Console.ReadLine());
+
+        for(int i = 0; i < ceny.Length; i++)
+        {
+            if(penize > ceny[i])
+                Console.WriteLine(hracky[i] + " (" + ceny[i] + ")");
+        }
+    }
+}
---
+++ uvodni-test/03-jednoduche-reseni/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.ConstrainedExecution;
+
+namespace _03_jednoduche_reseni;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Nacitani vstupu
+        Console.WriteLine("Kolik mas penez?");
+        double penize = Convert.ToDouble(Console.ReadLine());
+
+        // Zpracovani vstupu
+        Console.WriteLine("Muzes si dovolit nasledujici hracky:");
+
+        if(penize >= 75)
+            Console.WriteLine("Hot Wheels auticko (75)");
+
+        if(penize >= 120)
+            Console.WriteLine("Panenka (120)");
+        
+        if(penize >= 200)
+            Console.WriteLine("LEGO stavebnice (200)");
+    }
+}
