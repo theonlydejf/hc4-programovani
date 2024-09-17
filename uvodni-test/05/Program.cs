1e5971f87d10933c5d6238b23d8e1a00459f31ad
theonlydejf <krcmar.dav@gmail.com>
2024-09-17 13:43:16 +0200

    add solution for question 05

---
+++ uvodni-test/05/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _05;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Seznam znamych hracek
+        string[] hracky = new string[5];
+        // Seznam cen k prislusne hracce
+        int[] ceny = new int[5];
+
+        // Nacti inventar hrackarstvi
+        for(int i = 0; i < hracky.Length; i++)
+        {
+            Console.WriteLine((i + 1) + ". hracka:");
+            hracky[i] = Console.ReadLine();
+            Console.WriteLine("Cena " + (i + 1) + ". hracky:");
+            ceny[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.WriteLine("Kolik mas penez?");
+        double penize = Convert.ToDouble(Console.ReadLine());
+
+        // Projdi kazdou cenu hracky
+        for(int i = 0; i < ceny.Length; i++)
+        {
+            if(penize >= ceny[i]) // Pokud si hracku muzu dovolit
+                Console.WriteLine(hracky[i] + " (" + ceny[i] + ")"); // Vypis jeji jmeno a cenu
+        }
+    }
+}
