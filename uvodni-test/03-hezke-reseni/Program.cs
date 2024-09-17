3cf1d774a85875dcdf204988c2d9a38c29e8fca0
theonlydejf <krcmar.dav@gmail.com>
2024-09-17 13:14:33 +0200

    question 3 add docs to pretty solution

--- uvodni-test/03-hezke-reseni/Program.cs
+++ uvodni-test/03-hezke-reseni/Program.cs
@@ -8,13 +8,15 @@ class Program
         string[] hracky = new string[] { "LEGO stavebnice", "Hot Wheels auticko", "Panenka" };
         int[] ceny = new int[] { 200, 75, 120 };
 
+        // Zpracovani vstupu
         Console.WriteLine("Kolik mas penez?");
         double penize = Convert.ToDouble(Console.ReadLine());
 
+        // Projdi kazdou cenu
         for(int i = 0; i < ceny.Length; i++)
         {
-            if(penize > ceny[i])
-                Console.WriteLine(hracky[i] + " (" + ceny[i] + ")");
+            if(penize > ceny[i]) // Pokud si hracku muzu dovolit
+                Console.WriteLine(hracky[i] + " (" + ceny[i] + ")"); // Vypis jeji jmeno a cenu
         }
     }
 }
