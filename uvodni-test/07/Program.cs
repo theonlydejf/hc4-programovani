f6021204b3a69fcb4516618672eefbf976c42b97
theonlydejf <krcmar.dav@gmail.com>
2024-09-18 20:07:21 +0200

    add solution to question 07

---
+++ uvodni-test/07/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _07;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int cislo = 919;
+        if (JePalindrom(cislo))
+            Console.WriteLine("Je to palindrom");
+        else
+            Console.WriteLine("Neni to palindrom");
+    }
+
+    static bool JePalindrom(int cislo)
+    {
+        string cisloStr = cislo.ToString();
+
+        for(int i = 0; i < cisloStr.Length / 2; i++)
+        {
+            if(cisloStr[i] != cisloStr[cisloStr.Length - i - 1])
+                return false;
+        }
+
+        return true;
+    }
+}
