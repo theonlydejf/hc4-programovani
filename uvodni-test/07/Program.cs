406893f117a2a6cef10edd518480bdb043639864
theonlydejf <krcmar.dav@gmail.com>
2024-09-18 20:21:08 +0200

    add docs to solution for q. 07

--- uvodni-test/07/Program.cs
+++ uvodni-test/07/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Priklad z testu
         int cislo = 919;
         if (JePalindrom(cislo))
             Console.WriteLine("Je to palindrom");
@@ -13,10 +14,15 @@ class Program
 
     static bool JePalindrom(int cislo)
     {
-        string cisloStr = cislo.ToString();
+        string cisloStr = cislo.ToString(); // Preved cislo na text -> muzu prochazet jednotlive znaky
 
+        // Staci projit pulku cisla. Deleni dvou Integeru je zaokrouhlene vzdy dolu,
+        // tedy nevznika mi problem pri lichem poctu cifer. Prostredni cifru navic neni
+        // potreba kontrolovat, protoze to zda-li je cislo palindrom je na ni nezavisle
+        // (ofc v pripade sudeho poctu cifer na dvou prostrednich cifrach zalezi :D)
         for(int i = 0; i < cisloStr.Length / 2; i++)
         {
+            // Porovnej aktualni cifru s ni symetrickou, pokud nejsou stejne -> neni to palindrom.
             if(cisloStr[i] != cisloStr[cisloStr.Length - i - 1])
                 return false;
         }
