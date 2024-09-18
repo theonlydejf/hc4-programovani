b43feac775f960d7fe3161a0b7959f1d2e43db8d
theonlydejf <krcmar.dav@gmail.com>
2024-09-18 20:23:25 +0200

    add solution for question 08

---
+++ uvodni-test/08/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _08;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string text = "Why java programmers wear glasses? Beacuse they can't C#";
+        Console.WriteLine(SpoctiSlova(text));
+    }
+
+    static int SpoctiSlova(string text)
+    {
+        int pocet = 0;
+
+        char predchozi = ' '; // Mezera at se pocita prvni slovo
+        foreach(char znak in text)
+        {
+            if(predchozi == ' ' && znak != ' ') // Narazil jsi na zacatek slova?
+                pocet++;
+            predchozi = znak;
+        }
+
+        return pocet;
+    }
+}
